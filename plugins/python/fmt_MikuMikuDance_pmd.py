48fa39e3fa76737936c621331103977d92a4b3a5
johnwithlenon <46812978+johnwithlenon@users.noreply.github.com>
2022-07-19 07:40:51 +0700

    Update fmt_MikuMikuDance_pmd.py

--- plugins/python/fmt_MikuMikuDance_pmd.py
+++ plugins/python/fmt_MikuMikuDance_pmd.py
@@ -1,6 +1,7 @@
 # Modified from: https://github.com/atrzaska/noesis_py/blob/master/lib/plugins/fmt_MikuMikuDance_pmd.py
 # (Latest commit 08a638a on Nov 23, 2017)
-
+# Required Sanae3D in the same location
+#
 # 2022/07/19: Rotate model to front, and fix textures
 
 from inc_noesis import *
